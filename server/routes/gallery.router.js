d44195415016019f8c663777b21aaebb31191ecc
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-05-24 15:32:46 -0500

    PUT updates like count

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -4,7 +4,18 @@ const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
+  const photoID = req.params.id;
+  console.log('checking photoID', photoID);
+  const sqlText = `UPDATE gallery SET "likes" = likes+1 WHERE id=$1;`;
+  pool
+    .query(sqlText, [photoID])
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(`error liking post`, error);
+    });
 });
 
 // GET /gallery
